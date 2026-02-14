e74561cedccf7bcd4faf54d696f77b9cb9c5b4bd
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-02-14 15:34:53 +0000

    Add files via upload

--- nestfor8.c
+++ nestfor8.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main(){
-    for(int i=9;i>0;i--){
-        for(int j=1;j<4-i;j++){
-            printf("*");
-        }
-        for(int k=1;k<=2*i-1;k++){
+    for(int i=9;i>0;i-=2){
+        for(int s=9;s>i;s-=2){
             printf(" ");
         }
-        printf("\n");
+        for(int j=0;j<i;j++){
+            printf("*");
+        }
+         printf("\n");
     }
+  
     return 0;
 }
+
