baf7da0211cb95becdd5409ff4ec29d57bf2ee13
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-02-19 13:39:56 +0000

    Add files via upload

---
+++ while4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+    int N;
+    int status; 
+    int i=1;
+    int count_1=0;
+    int max =0;
+    scanf("%d",&N);
+    while(i<=N){
+        scanf("%d",&status);
+
+        if(status==0){
+          count_1++;
+
+        }
+        else if(status==1){
+            if(count_1>max){
+               max=count_1;
+            }
+            count_1=0;
+           
+        }
+    
+        i++;
+    } 
+    if(count_1>max){
+        max=count_1;
+    }
+    printf("Longest Failure Streak : %d",max);
+    return 0;
+    }
+    
+    
+    
