5aebb3b804121058ffbe36e6798189c956bdf32a
kavikamkavikam562-creator <kavikamkavikam562@gmail.com>
2026-03-01 13:33:49 +0000

    Add files via upload

---
+++ str10.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<ctype.h>
+void main(){
+    char str[50];
+    char a;
+    fgets(str,sizeof(str),stdin);
+    for(int i=0;str[i]!='\0';i++){
+        a=str[i];
+        putchar(tolower(a));
+       
+    }
+}
